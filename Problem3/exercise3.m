e55816d461ec3b3c7ae8593152af9b6b26663138
Lucas Leiradella <107800073+leiradella@users.noreply.github.com>
2025-05-16 01:36:27 +0200

    Added the code for the exercises 3 and 5 of part 3 of the project.

---
+++ Problem3/exercise3.m
@@ -0,0 +1,51 @@
+load("LP_Test.mat")
+
+g = [C; -U];
+A = [-ones(15,1); ones(30,1)];
+b = 0;
+l = [zeros(45,1)];
+u = [Pg_max; Pd_max];
+
+f = g;
+Aeq = A';
+beq = b;
+lb = l;
+ub = u;
+
+tic;
+options = optimoptions('linprog', 'Display', 'iter');
+[x_linprog, fval_linprog, exitflag, output, lambda] = ...
+    linprog(f, [], [], Aeq, beq, lb, ub, options);
+t_solve = toc;
+
+Pg = x_linprog(1:15);
+Pd = x_linprog(16:end);
+
+disp('Total generation (MW):'), disp(sum(Pg))
+disp('Total demand (MW):'), disp(sum(Pd))
+disp('Market clearing price (€/MW):'), disp(lambda.eqlin)  % dual variable of Aeq
+
+sorted_C = sort(C);                % ascending for supply
+sorted_U = sort(U,'descend');      % descending for demand
+
+cum_Pg = cumsum(Pg_max);           % total capacity per generator
+cum_Pd = cumsum(Pd_max);           % total capacity per demand
+
+figure;
+hold on;
+stairs([0; cum_Pg], [0; sorted_C], 'b', 'LineWidth', 2)    % supply
+stairs([0; cum_Pd], [0; sorted_U], 'r', 'LineWidth', 2)    % demand
+xlabel('Energy quantity (MW)')
+ylabel('Price (€/MW)')
+legend('Supply', 'Demand')
+title('Market Clearing Supply-Demand Curve')
+grid on;
+
+fprintf('Solver stats:\n');
+fprintf('Iterations: %d\n', output.iterations);
+fprintf('CPU Time: %.4f seconds\n', t_solve);
+
+print(gcf, 'supply_demand_plot', '-dpng', '-r300');  % 300 dpi for high quality
+
+
+
---
+++ Problem3/exercise5.m
@@ -0,0 +1,105 @@
+close all; clear all;
+
+function x_sol = InteriorPoint(g, A, b, l, u, x0, epsilon)
+    %% convert to standard form
+    %get the size of x
+    n = length(x0);
+
+    % make the slack variable vector for all inequalities x <= u
+    s = ones(n, 1);
+
+    %now we update the variables to be in standard form
+    A = [A', zeros(1, n)
+             eye(n), eye(n)];
+
+    b = [b;u];
+
+    g = [g; zeros(n, 1)];
+
+    x = [x0;s];
+
+    %the program is now in standard LP form
+    % min g'x; A'x = b; x>=0;
+
+    %% interior point algorithm
+
+    lambda = ones(size(x));
+    mu = zeros(size(b));
+
+    for k = 0:100
+
+        %compute the duality measure
+        s = (x'*lambda)/length(x);
+
+        %centering parameter
+        sigma = 0.1;
+
+        %compute residuals
+        rL = g - A'*mu - lambda;
+        rA = A*x - b;
+
+        %check convergence
+        fprintf('Iteration %3i, |r_L| = %6.4e, |r_A| = %6.4e \n', k, norm(rL), norm(rA))
+        if norm(rL) < epsilon && norm(rA) < epsilon && abs(s) < epsilon
+            fprintf("Converged\n")
+            break
+        end
+
+        %solve the newton step
+        lenx = length(x);
+        lenA = size(A, 1);
+
+        diaglambda = diag(lambda);
+        diagX = diag(x);
+
+        KKT = [zeros(lenx),   -A',             -eye(lenx);
+               A,          zeros(lenA),      zeros(lenA, lenx);
+               diaglambda, zeros(lenx,lenA),       diagX];
+
+        e = ones(lenx, 1);
+
+        rhs = [-rL; -rA; -diagX*diaglambda*e+ sigma*s*e];
+
+        sol = KKT \ rhs;
+
+        dx = sol(1:length(rL));
+        dmu = sol(length(rL)+1:length(rL)+length(rA));
+        dlambda = sol(length(rL)+length(rA)+1:end);
+
+        %compute alpha such that 
+        %x + dx*alpha > 0 and lambda + dlambda*alpha > 0
+        alpha = [-x./dx;-lambda./dlambda];
+        num_zeros=alpha([dx<0;dlambda<0]);
+        if size(num_zeros,1)==0 
+            alpha = 0;
+        else
+            alpha = min(num_zeros);
+        end
+
+        alpha = alpha*0.99;
+
+        x = x + dx*alpha;
+        mu = mu + dmu*alpha;
+        lambda = lambda + dlambda*alpha;
+    end
+    x_sol = x;
+end
+
+
+%load variables for the problem
+load("LP_Test.mat");
+
+
+
+Cn = length(C);
+Un = length(U);
+
+g = [C;-U];
+A = [-ones(Cn,1);ones(Un,1)];
+b = 0;
+u = [Pg_max; Pd_max];
+l = [zeros(size(u))];
+x0 = [(Un/Cn)*ones(Cn,1);ones(Un,1)];
+epsilon = 1e-8;
+
+x = InteriorPoint(g, A, b, l, u, x0, epsilon);
