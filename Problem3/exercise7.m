e956d0d7226ed4da3b23328b4a90cedb5c53b809
Lucas Leiradella <107800073+leiradella@users.noreply.github.com>
2025-05-17 19:28:58 +0200

    Added feasible point calculation to the simplex algorithm in proble 3. Currently not working

--- Problem3/exercise7.m
+++ Problem3/exercise7.m
@@ -1,49 +1,77 @@
 close all; clear all;
 
+function [g_std, A_std, b_std] = LPstandardForm(g, A, b, u)
+    %convert the lp problem into standard form
 
-function x_sol = simplex(g, A, b, l, u, x0)
-    %% convert to standard form
-    %get the size of x
-    n = length(x0);
-
-    % make the slack variable vector for all inequalities x <= u
-    s = ones(n, 1);
+    [m, n] = size(A);
 
     %now we update the variables to be in standard form
-    A = [A', zeros(1, n)
-             eye(n), eye(n)];
+    g_std = [g; zeros(m, 1)];
 
-    b = [b;u];
-    g = [g; zeros(n, 1)];
-    x = [x0;s];
+    A_std = [A', zeros(n, m)
+             eye(m), eye(m)];
 
-    %% simplex algorithm
+    b_std = [b;u];
+end
+
+% function x0 = feasiblePoint(A, b)
+%     [m, n] = size(A);
+%     f = [zeros(n,1); 1];                        % objective: min t
+%     A_lp = [ A, -ones(m,1);                   % Ax - t <= b
+%             -A, -ones(m,1) ];                 % -Ax - t <= -b
+%     b_lp = [ b; -b ];
+% 
+%     lb = [zeros(n,1); 0];                      % x ≥ 0, t ≥ 0
+% 
+%     z = linprog(f, A_lp, b_lp, [], [], lb, []);
+%     x0 = z(1:n);
+%     t_opt = z(end);
+% 
+%     if t_opt > 0
+%         error("not feasible")
+%     end
+% end
+
+function x0 = feasiblePoint(A, b)
+
+    [m, n] = size(A);
+
+
+    %make x0 = [x; t; s1; s2]
+    x = zeros(n, 1);
+
+    t = max(abs(b));
+
+    s1 = t*ones(m, 1) - b;
+    s2 = t*ones(m, 1) + b;
+
+    x0 = [x; t; s1; s2];
+
+    %create g so that it minimizes t only
+    g = [zeros(n, 1); 1; zeros(2*m, 1)];
+
+    %create A to satisfy the new equality conditions
+    % Ax + t >= +-b     Ax + t - s = +-b
+    A = [A, ones(m, 1), -eye(m), zeros(m);
+        -A, ones(m, 1), zeros(m), -eye(m)];
+
+    %find the feasible point using simplex
+    x0 = simplex(g, A, x0);
+    t = x0(n+1);
+    x0 = x0(1:n);
+
+    if t > 1e-6
+        error("Infeasible")
+    end
+end
+
+function x_sol = simplex(g, A, x)
+    
     %create set N of all i where xi = 0
     %and set B with all the other xi (xi > 0)
     Fullset = 1:length(x);
     Nset = find(x==0);
     Bset = setdiff(Fullset,Nset);
-
-    %remake the A matrix as well
-    Bmat = A(:, Bset);
-    Nmat = A(:, Nset);
-
-    A = [Bmat, Nmat];
-
-    %calcualte xb
-    xb = Bmat \ b;
-    xn = zeros(length(Nset), 1);
-
-    %reorder x to be [xb;xn]
-    x = zeros(length(x), 1);
-    x(Bset) = xb;
-    x(Nset) = xn;
-
-    %set lambda accordingly
-    %for all xb lambda = 0 and for all xn lambda >= 0
-    lambda = zeros(length(x), 1);
-    lambda(Nset) = 1;
-    lambda(Bset) = 0;
 
     for k = 0:100
         xB = x(Bset);
@@ -53,8 +81,10 @@ function x_sol = simplex(g, A, b, l, u, x0)
         mu = B'\g(Bset);
         lambdaN = g(Nset) - N'*mu;
 
+        fprintf("t = %.4f\n", x(90+1));
+
         if all(lambdaN>=0)
-            fprintf("Problem converged\n")
+            fprintf("iteration %i: problem converged\n", k)
             break
         end
         
@@ -63,9 +93,8 @@ function x_sol = simplex(g, A, b, l, u, x0)
         idx = 1:length(xB);
         idx = idx(h>0);
         sol = xB(h>0)./h(h>0);
-        min_sol = min(sol);
-        J = min_sol(2);
-        J = index(J);
+        [~, J] = min(sol);
+        J = idx(J);
         if J == []
             error("Unbound problem, no solution\n")
             return;
@@ -97,6 +126,16 @@ A = [-ones(Cn,1);ones(Un,1)];
 b = 0;
 u = [Pg_max; Pd_max];
 l = [zeros(size(u))];
-x0 = [(Un/Cn)*ones(Cn,1);ones(Un,1)];
 
-x = simplex(g, A, b, l, u, x0);
+
+%transform problem into standard form
+[g, A, b] = LPstandardForm(g, A, b, u);
+
+%get feasible point (using simplex)
+fprintf("phase 1\n")
+x0 = feasiblePoint(A, b);
+
+
+%get an optimal point (using simplex)
+fprintf("phase 2\n")
+x = simplex(g, A, x0);
